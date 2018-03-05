7a1995ee58af296b3346f9d3dbd7adbf1664b272
Marica Odagaki <ento.entotto@gmail.com>
2018-03-05 00:51:14 -0800

    Make it possible to use remapper/ as-is

--- remapper/main.js
+++ remapper/main.js
@@ -1,6 +1,5 @@
 (function() {
   var remapper = new Remapper.Engine(keymap);
-  chrome.input.ime.onFocus.addListener(remapper.handleFocus.bind(remapper));
-  // use a different property access method as a testcase of the transformer script
-  chrome.input.ime["onKeyEvent"].addListener(remapper.handleKeyEvent.bind(remapper));
+  Remapper.hijack.onFocus.addListener(remapper.handleFocus.bind(remapper));
+  Remapper.hijack.onKeyEvent.addListener(remapper.handleKeyEvent.bind(remapper));
 })();
